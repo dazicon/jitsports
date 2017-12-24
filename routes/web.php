209d568c524e48e0e7afb5c76bf947d9d8dbbe52
hanhongda <hansir1993@outlook.com>
2017-12-24 03:37:17 +0000

    move user model to models folder

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,8 @@
 */
 
 //Route::get('/', function () {return view('welcome');});
-//Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
-//Route::get('signup','UsersController@create')->name('signup');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('signup','UsersController@create')->name('signup');
