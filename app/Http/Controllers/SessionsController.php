789c8ed8e37a8e8c7c7ecfeac7fe70033432c87d
hanhongda <hansir1993@outlook.com>
2017-12-30 08:04:52 +0000

    Finish user edit,update and authorize

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        //只让未登录用户访问登录页面
+        $this->middleware('guest',[
+            'only' => ['create']
+        ])
+    }
     /**
      * 进入登录页面
      */
@@ -29,7 +36,7 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials,$request->has('remember'))) {
            session()->flash('success','欢迎回来!');
-           return redirect()->route('users.show',[Auth::user()]);
+           return redirect()->intended(route('users.show',[Auth::user()]));
        } else {
            session()->flash('danger','sorry,您的邮箱与密码不匹配');
            return redirect()->back();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,22 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        //except指定不使用中间件过滤的动作
+        $this->middleware('auth',[
+            'except' => ['show','create','store']
+        ]);
+
+        //只让未登录用户访问注册页面
+        $this->middleware('guest',[
+            'only' => ['create']  
+        ]);
+    }
+
+    /**
+     * 进入注册页面
+     */
     public function create()
     {
         return view('users.create');
@@ -17,10 +33,10 @@ class UsersController extends Controller
     /**
      * 显示用户个人主页
      */
-     public function show(User $user)
-     {
-         return view('users.show', compact('user'));
-     }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
 
     /**
      * 用户注册
@@ -47,5 +63,47 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
+    }
+
+    /**
+     * 进入编辑用户页面
+     */
+    public function edit(User $user)
+    {
+        //授权验证
+        $this->authorize('update','$user');
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * 更新提交的用户信息
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+           'name' => 'required|max:50',
+           'stu_id' => 'required|numeric|min:10',
+           'class' => 'required|max:100',
+           //required 规则换成 nullable，当用户提供空白密码时也会通过验证,不必要改密码
+           'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        //授权验证
+        $this->authorize('update','$user');
+
+        $data = [];
+        $data['name'] = $request->name;
+        $date['stu_id'] = $request->stu_id;
+        $date['class'] = $request->class;
+
+        //对传入的 password 进行判断，当其值不为空时才将其赋值给 data，避免将空白密码保存到数据库中。
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show',$user->id);
     }
 }
